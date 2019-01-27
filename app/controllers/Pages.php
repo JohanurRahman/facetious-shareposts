e708ffa5889b29ff069ca72a12fb373a582684b3
JohanurRahman <johanur.rahman@gmail.com>
2019-01-27 16:12:50 +0600

    Added facetious framework

---
+++ app/controllers/Pages.php
@@ -0,0 +1,30 @@
+<?php
+
+    class Pages extends Controller {
+
+        public function __construct() {
+
+        }
+        
+        public function index() {
+
+            $data = [
+                'title' => 'Facetious MVC'
+            ];
+
+            $this->view('pages/index', $data);
+            
+        }
+
+        public function about() {
+            $data = [
+                'title' => 'About us'
+            ];
+            $this->view('pages/about', $data);
+            
+        }
+        
+    }
+
+
+?>
