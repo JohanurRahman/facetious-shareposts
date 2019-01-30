f768766682a7a08862810baba279c304b58feea2
JohanurRahman <johanur.rahman@gmail.com>
2019-01-30 15:35:39 +0600

    Posts Controller, Access Control, Model and Display Added

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -33,10 +33,16 @@
 
     }
 
-
-    $_SESSION['user'] = 'Brad';
     
-    unset($_SESSION['user']);
+    function isLoggedIn() {
+        if(isset($_SESSION['user_id'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 
 
     
